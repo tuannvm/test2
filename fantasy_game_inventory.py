6b79d1c729186a0aa721c99f34ca55414af064c0
Tuan Nguyen <tuanpro.itman@gmail.com>
2016-04-13 00:52:02 +0700

    test commit

---
+++ fantasy_game_inventory.py
@@ -0,0 +1,25 @@
+stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def displayInventory(inventory):
+    print("Inventory:")
+    item_total = 0
+    for k, v in inventory.items():
+        item_total = item_total + v
+    print("Total number of items: " + str(item_total))
+
+displayInventory(stuff)
+
+def addToInventory(inventory, addedItems):
+    for i in range(len(addedItems)):
+        inventory.setdefault(addedItems[i],0)
+        for stuff in inventory:
+            if stuff == addedItems[i]:
+                inventory[stuff] +=1
+    return inventory
+
+
+inv = {'gold coin': 42, 'rope': 1}
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(inv, dragonLoot)
+displayInventory(inv)
+print inv
